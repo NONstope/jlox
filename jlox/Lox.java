6caf416360e713d55261984569bc88b45785d4c3
Daniel <khodienkod@gmail.com>
2023-07-29 17:56:43 -0400

    Created a token scanner

--- jlox/Lox.java
+++ jlox/Lox.java
@@ -1,9 +1,12 @@
+package jlox;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Lox {
 
@@ -45,7 +48,13 @@ public class Lox {
     }
 
     private static void run(String source) {
-        System.out.println("Doing some magic");
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        // For now, just print the tokens.
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
     }
 
     static void error(int line, String message) {
